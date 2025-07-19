e0c30fcb6a96a4c90f07f8869de6660353d43fdd
tipsypastels <dakotasankey@gmail.com>
2025-07-18 18:50:32 -0700

    core?

--- tui-core/src/lib.rs
+++ tui-core/src/lib.rs
@@ -1,8 +1,28 @@
 mod building;
 mod upgrade;
 
-pub use building::*;
-pub use upgrade::*;
+#[derive(Debug)]
+pub struct Core {
+    state: State,
+    cached: Cached,
+}
 
 #[derive(Debug)]
-pub struct Core {}
+struct State {}
+
+#[derive(Debug)]
+struct Cached {
+    cps: f64,
+}
+
+impl Core {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    pub fn cps(&self) -> f64 {
+        self.cached.cps
+    }
+
+    pub fn tick(&mut self, fps: f64) {}
+}
